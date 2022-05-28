b6369883999a9b1b31ab9c6cb0c132c05c3b5cb9
abduljamalbasha <azeezjamal6@gmail.com>
2022-05-28 18:49:19 +0530

    code-splitting added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import React, { Suspense } from 'react';
 import Header from './components/Header';
 // import {
 //   BrowserRouter as Router,
@@ -8,22 +9,28 @@ import Header from './components/Header';
 // } from "react-router-dom";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import ProductListing from './components/ProductListing';
-import ProductDetails from './components/ProductDetail';
+import ErrorFallback from './components/ErrorBoundry';
+import { ErrorBoundary } from 'react-error-boundary';
+const ProductListing = React.lazy(()=>import('./components/ProductListing'))
+const ProductDetails = React.lazy(()=> import('./components/ProductDetail'))
 
 function App() {
   return (
     <div className="App">
-
-    <Router>
-    <Header />
-      <Routes>
-        <Route path="/" caseSensitive={false} element={<ProductListing />} />
-        <Route path="/product/:productId" caseSensitive={false} element={<ProductDetails />} />
-        <Route >404 page found</Route>
-      </Routes>
-    </Router>
+      <Header />
+      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=>{}}>
+      <Suspense fallback={<div>Loading produuct please wait....</div>}>
+          <Router>
+          
+            <Routes>
+              <Route path="/" caseSensitive={false} element={<ProductListing />} />
+              <Route path="/product/:productId" caseSensitive={false} element={<ProductDetails />} />
+              <Route >404 page found</Route>
+            </Routes>
+          </Router>
+      </Suspense>
+      </ErrorBoundary>
+      
     </div>
   );
 }
